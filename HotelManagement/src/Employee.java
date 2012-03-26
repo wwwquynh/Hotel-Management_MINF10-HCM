cf0f63098fbb90a7bcf82a0257225dbe4112d53e
rukowen <thehien811@gmail.com>
2012-03-26 20:07:38 +0700

    rep

--- HotelManagement/src/Employee.java
+++ HotelManagement/src/Employee.java
@@ -2,7 +2,8 @@ import java.util.ArrayList;
 
 
 public interface Employee {
-	public int addEmployee();
+	public int addEmployee(Employee e);
 	public Employee getEmployee(int empID);
 	public ArrayList<Employee> searchEmployee();
+	public int updateCustomer(Employee e);
 }
