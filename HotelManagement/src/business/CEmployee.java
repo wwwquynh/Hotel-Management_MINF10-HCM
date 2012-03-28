c9597ca2c7730b93c085891332bcd4c5287ea354
rukowen <thehien811@gmail.com>
2012-03-28 07:41:32 +0700

    add emp

---
+++ HotelManagement/src/business/CEmployee.java
@@ -0,0 +1,43 @@
+package business;
+
+import java.util.ArrayList;
+
+public class CEmployee implements Employee{
+
+	@Override
+	public int addEmployee(Employee e) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Employee getEmployee(int empID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Employee> searchEmployee() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateEmployee(Employee e) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateStatus(int status) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int assignTask(ArrayList<Task> tasks) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
