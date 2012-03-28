587bb17b8b87587de444fb1ba2971be3614f4685
rukowen <thehien811@gmail.com>
2012-03-28 14:03:35 +0700

    commit EMP

--- HotelManagement/src/business/Employee.java
+++ HotelManagement/src/business/Employee.java
@@ -7,7 +7,7 @@ public interface Employee {
 	public boolean addEmployee();
 	public Employee getEmployee(int empID);
 	public ArrayList<Employee> searchEmployee();
-	public int updateEmployee(Employee e);
+	public int updateEmployee();
 	public int updateStatus(int status);
 	public int assignTask(ArrayList<Task> tasks);
 
