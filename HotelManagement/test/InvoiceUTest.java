0a0cb285262236a37b80448d53e38ade0bea214c
NgocQuynh <PCLT@pclt>
2012-04-16 10:24:12 +0700

    Test Case

---
+++ HotelManagement/test/InvoiceUTest.java
@@ -0,0 +1,21 @@
+
+import static org.junit.Assert.*;
+import org.easymock.EasyMock;
+import org.junit.Test;
+
+import business.InvoiceManager;
+import connect.sqlite.IConnectData;
+
+
+
+public class InvoiceUTest {
+	//	addNewInvoice()
+	@Test(expected=NullPointerException.class)
+	public void addNewCustomer_NullParamTest(){
+		IConnectData mockconnection = EasyMock.createNiceMock(IConnectData.class);
+		
+		InvoiceManager obj = new InvoiceManager(mockconnection);
+		assertFalse( obj.addNewInvoice(null));
+	}
+	
+}
