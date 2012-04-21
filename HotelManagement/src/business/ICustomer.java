73545f7e226d1fe6e95120f755303cfb19afeb44
rukowen <thehien811@gmail.com>
2012-04-21 18:01:52 +0700

    svn support - remove old ones

--- HotelManagement/src/business/ICustomer.java
+++
@@ -1,9 +0,0 @@
-package business;
-
-public interface ICustomer {
-	public boolean addCustomer();
-	public boolean updateCustomer();
-	public boolean deleteCustomer();
-	public int getRowCount();
-	public Object getValueAt(int row, int column);
-}
